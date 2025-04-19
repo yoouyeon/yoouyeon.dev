ec47c9f697a58a0d6d6d3052c1fea7923660ad38
yoouyeon <jyeon.yoon59@gmail.com>
2025-04-19 22:39:57 +0900

    refactor: MDXLayout 컴포넌트를 article 태그로 변경

--- src/components/MdxLayout.tsx
+++ src/components/MdxLayout.tsx
@@ -3,9 +3,5 @@ type MdxLayoutProps = {
 };
 
 export default function MdxLayout({ children }: MdxLayoutProps) {
-  return (
-    <div className="mdx prose dark:prose-invert max-w-none pb-16">
-      {children}
-    </div>
-  );
+  return <article className="mdx pb-16">{children}</article>;
 }
