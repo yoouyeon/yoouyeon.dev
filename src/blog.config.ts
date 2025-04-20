8f16c8a18cf86f7d19a7a272185fe575f1b3ed80
Yoon Jeongyeon <jyeon.yoon59@gmail.com>
2025-04-20 17:10:47 +0900

    메타 데이터를 수정하고 추가하다 (#3)
    * readme 업데이트
    
    * og image
    
    * 블로그 소개글 수정
    
    * 위키 링크 업데이트
    
    * 기본 robots.txt 추가

--- src/blog.config.ts
+++ src/blog.config.ts
@@ -2,9 +2,11 @@ export const profileMetadata = {
   siteTitle: "yoouyeon.dev",
   authorKo: "유연",
   authorEn: "yoouyeon",
-  description: "블로그 만들어요.",
+  description:
+    "개발을 통해 아이디어를 실현하는 걸 좋아합니다. 복잡하고 번거로운 일을 단순화할 때 큰 보람을 느낍니다. 생각과 배움을 글로 정리했을 때 더 깊이 이해할 수 있어 꾸준히 기록으로 남기고 있습니다. 저의 경험이 누군가에게 도움이 되길 바라며 블로그에 글을 씁니다.",
   email: "jyeon.yoon59@gmail.com",
   github: "https://github.com/yoouyeon",
+  wiki: "https://notes.yoouyeon.dev",
 } as const;
 
 export const siteConfig = {
