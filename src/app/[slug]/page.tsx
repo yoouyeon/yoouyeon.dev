7ee4a05f66a2629effb0a4ccd3e7b691cae9a8f4
yoouyeon <jyeon.yoon59@gmail.com>
2025-04-22 15:09:56 +0900

    feat: 글 메타데이터 생성 기능 추가

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import Link from "next/link";
 import dayjs from "dayjs";
 import { ArrowUturnLeftIcon } from "@heroicons/react/16/solid";
@@ -20,6 +21,20 @@ export async function generateStaticParams() {
 type PostProps = {
   params: Promise<{ slug: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: PostProps): Promise<Metadata> {
+  const { slug } = await params;
+  const { meta } = parseMdx(`src/content/posts/${slug}/index.mdx`);
+  const { frontmatter } = meta;
+  const { title, description } = frontmatter;
+
+  return {
+    title,
+    description,
+  };
+}
 
 export default async function Post({ params }: PostProps) {
   const { slug } = await params;
