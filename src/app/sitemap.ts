7e565457958d85c9108bf9b72d3d1c12bf7390ab
yoouyeon <jyeon.yoon59@gmail.com>
2025-04-22 15:37:41 +0900

    feat: 정적 페이지를 사이트맵에 추가

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,12 +2,17 @@ import type { MetadataRoute } from "next";
 import { BLOG_LINK } from "@/config/blog";
 import { getAllPosts } from "@/libs/getAllPosts";
 
+const staticSitemap: MetadataRoute.Sitemap = [
+  { url: BLOG_LINK.BASE, lastModified: new Date() },
+];
+
 // NOTE : url이 50000개가 넘는 경우에는 사이트맵을 쪼개야 한다는 것을 기억하기
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getAllPosts();
-
-  return posts.map((post) => ({
+  const postSitemap = posts.map((post) => ({
     url: `${BLOG_LINK.BASE}/${post.slug}`,
     lastModified: new Date(),
   }));
+
+  return [...staticSitemap, ...postSitemap];
 }
