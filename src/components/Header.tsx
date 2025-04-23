e318133e2fe81cd1754c5566aabb1aad8d56f10a
yoouyeon <jyeon.yoon59@gmail.com>
2025-04-23 13:55:48 +0900

    design: 헤더 링크 underline 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
     <div className="flex items-center justify-between gap-2 mb-4">
       <Link
         href={"/"}
-        className="text-mute w-full hover:underline hover:underline-offset-4 hover:decoration-1 hover:decoration-mute/30"
+        className="text-mute w-full no-underline hover:underline hover:underline-offset-4 hover:decoration-1 hover:decoration-mute/30"
       >
         {BLOG_PROFILE.TITLE}
       </Link>
