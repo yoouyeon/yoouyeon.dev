e298d63c2cfa0dd4c8d697535919d74f4c96901d
yoouyeon <jyeon.yoon59@gmail.com>
2025-04-23 13:50:02 +0900

    design: 404 페이지 디자인 수정
    - 페이지 디자인 수정
    - 사용하지 않는 파일 제거

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,32 +1,21 @@
+import PageHeader from "@/components/PageHeader";
+import Button from "@/components/ui/Button";
+import { ArrowUturnLeftIcon } from "@heroicons/react/16/solid";
 import Link from "next/link";
-import styles from "./page.module.css";
 
 export default function NotFound() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>404 Not Found</h1>
-      <p className={styles.description}>
-        이 페이지는 존재하지 않거나 아직 준비되지 않았어요.
-      </p>
-      <Link className={styles.link} style={{ marginTop: "2rem" }} href="/">
-        {/* undo 아이콘 */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-undo-icon lucide-undo"
-        >
-          <path d="M3 7v6h6" />
-          <path d="M21 17a9 9 0 0 0-9-9 9 9 0 0 0-6 2.3L3 13" />
-        </svg>
-        메인 페이지로 돌아가기
-      </Link>
-    </main>
+    <>
+      <PageHeader
+        title={"404 Not Found"}
+        description={"페이지를 찾을 수 없습니다."}
+      />
+      <Button asChild className="mt-8">
+        <Link href={"/"}>
+          <ArrowUturnLeftIcon className="w-4 h-4" />
+          홈으로 돌아가기
+        </Link>
+      </Button>
+    </>
   );
 }
