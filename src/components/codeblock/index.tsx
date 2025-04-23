75ee9ab8ea46874b6e260cfebf9f6890a6bdf28c
yoouyeon <jyeon.yoon59@gmail.com>
2025-04-23 10:37:28 +0900

    design: 코드블록 다크모드 컬러 수정

--- src/components/codeblock/index.tsx
+++ src/components/codeblock/index.tsx
@@ -10,7 +10,7 @@ async function CodeBlock({ codeblock }: CodeBlockProps) {
   const highlighted = await highlight(codeblock, "material-from-css");
 
   return (
-    <div className="border border-mute/50 font-mono text-sm leading-6 overflow-auto bg-[#fafafa] rounded-md py-4 mb-5">
+    <div className="border border-mute/50 font-mono text-sm leading-6 overflow-auto bg-[#fafafa] dark:bg-[#263238] rounded-md py-4 mb-5">
       <Pre code={highlighted} handlers={[lineNumbers]} />
     </div>
   );
